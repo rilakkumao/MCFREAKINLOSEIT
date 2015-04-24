f72f4a472d7d53d4a618f8bc822ab786f3eca75a
Tallowy <djdoesthedo@aol.com>
2015-04-24 07:49:02 -0400

    Added JFrame and added Drawer class to it
    Drawer class not yet created.

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -1,8 +1,19 @@
 package javaapplication20;
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class JavaApplication20 {
 
     public static void main(String[] args) {
-    }
-    
+        JFrame frame = new JFrame();
+        frame.setSize(1000,1000);
+        frame.setTitle("Warcraft 4");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+        frame.add(new Drawer());
+    }        
 }
